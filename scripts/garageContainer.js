e318147327668d30ebeec972682bcbc4c5cff9c1
OwerHub <owerwild@gmail.com>
2022-01-04 02:59:39 +0100

    Kisebb képek OK

--- scripts/garageContainer.js
+++ scripts/garageContainer.js
@@ -3,7 +3,7 @@
 const try2 = "tryPic";
 
 // kiszervezni, már ha a mime type nem baszódik meg
-
+// garage azonosítója,
 function garageIdentifier(oneGarage, garageDatas) {
   const garageIdentifierDiv = document.createElement("div");
   oneGarage.append(garageIdentifierDiv);
@@ -30,62 +30,22 @@ function garageIdentifier(oneGarage, garageDatas) {
 }
 
 //---
-/* function carWrapper(carDatas, oneGarage) {
-  const carsWrapper = document.createElement("div");
-  oneGarage.append(carsWrapper);
-  carsWrapper.classList.add("carsWrapper");
 
-  carDatas.forEach((element) => {
-    const carContainer = document.createElement("div");
-    carsWrapper.append(carContainer);
-    carContainer.classList.add("carContainer");
-
-    const carPicDiv = document.createElement("div");
-    carContainer.append(carPicDiv);
-    carPicDiv.classList.add("carPicDiv");
-
-    const carPic = document.createElement("img");
-    carPicDiv.append(carPic);
-    carPic.classList.add("carPic");
-    //carPic.src = `../img/${try2}.jpg`;
-    carPic.src = "../img/tryPic.jpg";
-    const carDatasWrapper = document.createElement("div");
-    carContainer.append(carDatasWrapper);
-    carDatasWrapper.classList.add("carDatasWrapper");
-
-    const dataTryArray = ["ford", "escort"];
-    const dataNamesArray = ["gyártó", "típus", "változat"];
-
-    console.log(element.datas);
-    function createCarDatas(dataNames, dataValues) {
-      for (let index = 0; index < dataNames.length; index++) {
-        let currentDataValue;
-        if (!dataValues[index]) {
-          currentDataValue = "n.a";
-        } else {
-          currentDataValue = dataValues[index];
-        }
-        const carData = document.createElement("div");
-        carDatasWrapper.append(carData);
-        carData.classList.add("carData");
-        carData.innerHTML = `<span>${dataNames[index]} :</span> <span>${currentDataValue}</span>`;
-      }
-    }
-
-    createCarDatas(dataNamesArray, element.datas);
-    //carDatasWrapper.classList.add("carDatasWrapper");
-  });
-} */
+// ---- carsWrapper, ide renderelem ki a kocsikat
 
 function carWrapper(carDatas, oneGarage, carDatas2) {
   const carsWrapper = document.createElement("div");
 
   oneGarage.append(carsWrapper);
   carsWrapper.classList.add("carsWrapper");
+  const fragment = new DocumentFragment();
+
+  const infragment = document.createElement("div");
   carDatas2.forEach((element) => {
     const nameArray = element.split("-");
     //console.log(nameArray);
 
+    infragment.classList.add("infragment");
     const carCardHTML = `
     <div class="carContainer">
       <div class="carPicDiv">
@@ -108,8 +68,11 @@ function carWrapper(carDatas, oneGarage, carDatas2) {
       </div>
     `;
 
-    carsWrapper.insertAdjacentHTML("afterbegin", carCardHTML);
+    infragment.insertAdjacentHTML("afterbegin", carCardHTML);
+    fragment.append(infragment);
   });
+
+  carsWrapper.append(fragment);
 }
 
 //-------------------------------------MAIN----
